a76e94a16408339f21b5dbf03f044c19c2e63c5f
tsigemariamzewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2024-05-13 16:14:33 +0300

    question 1 new.py

--- question 1 new.py
+++ question 1 new.py
@@ -1,19 +1,14 @@
 def number1program():
-    newlist=[]
-    newdict={}
-    newlist1=[]
-    
-    numbers=input("enter the elements:")
-    target=int(input("which element are you searchingn for : "))
-    newnum=numbers.split(",")
-    for i in newnum:
-        newlist.append(int(i))
-    for i in newlist:
-        count=0
-        
-        if i==target:
-            count += 1
-    return count     
-        
+    numbers = input("Enter the elements: ")
+    target = int(input("Which element are you searching for: "))
 
-number1program()
+    newlist = numbers.split(",")
+    count = 0
+
+    for i in newlist:
+        if int(i) == target:
+            count += 1
+
+    return count
+
+print(number1program())
